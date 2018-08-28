c4e933848f56e3bdce4566e4cffa6f2d51d87215
Meghan.Guse <meghan.guse@gmail.com>
2018-08-28 17:32:15 -0500

    initial html

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Learn ALL THE THINGS</h1>
+    <p>
+      <img src="https://i.imgflip.com/4exm.jpg" style="width: 200px;" />
+    </p>
+  </body>
+</html>
